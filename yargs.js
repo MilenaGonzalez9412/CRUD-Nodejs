00f1a33575276c47252f585dc68f36329068d4aa
MilenaGonzalez9412 <samy.9412@gmail.com>
2019-03-28 10:12:43 -0500

    First commit

---
+++ yargs.js
@@ -0,0 +1,28 @@
+const nombre = {
+    demand: ture,
+    alias: 'n'
+}
+
+const matematicas = {
+    demand: true,
+    alias: 'm'
+}
+
+const ingles = {
+    demand: true,
+    alias: 'i'
+}
+
+const programacion ={
+    demand: true,
+    alias: 'p'
+}
+
+const creacion ={
+    nombre, 
+    matematicas,
+    ingles,
+    programacion
+}
+
+const arg = require('yargs')
